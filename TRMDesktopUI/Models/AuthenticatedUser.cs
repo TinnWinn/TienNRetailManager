bf64ad2b84ce9c376b75046a8563333a38cc49d2
Tien Nguyen <TienNguyen0530@hotmail.com>
2020-07-27 21:24:35 -0500

    Create and Wire Up Login Form to API
    Create WPF xaml for view and connects the login form button to the authentication API endpoint (/token). Gets back the bearer token or an exception if failed.

---
+++ TRMDesktopUI/Models/AuthenticatedUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xaml;
+
+namespace TRMDesktopUI.Models
+{
+    public class AuthenticatedUser
+    {
+        public string access_token { get; set; }
+        public string userName { get; set; }
+    }
+}
